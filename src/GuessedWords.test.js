d2b755f12b48f13c6273c067706c6f74075cd4c3
MarcinBalejko <mbbm9@o2.pl>
2021-01-21 23:31:47 +0100

    :white_check_mark: Added GuessedWords component's test setup and proptype test

---
+++ src/GuessedWords.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { findByTestAttr, checkProps } from "../test/testUtils";
+import GuessedWords from "./GuessedWords";
+
+const defaultProps = {
+  guessedWords: [{ guessedWord: "train", letterMatchCount: 3 }],
+};
+
+const setup = (props = {}) => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<GuessedWords {...setupProps} />);
+};
+
+test("does not throw warning with expected props", () => {
+  checkProps(GuessedWords, defaultProps);
+});
