ec0b6fada124c641cdf3c189edc5bcd23fae5c6e
MarcinBalejko <mbbm9@o2.pl>
2021-01-21 03:05:31 +0100

    :recycle: Extracted enzyme setup to setupTests.js file

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,10 +1,7 @@
 import React from "react";
-import Enzyme, { shallow } from "enzyme";
-import EnzymeAdapter from "enzyme-adapter-react-16";
+import { shallow } from "enzyme";
 import { findByTestAttr, checkProps } from "../test/testUtils";
 import Congrats from "./Congrats";
-
-Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 const defaultProps = { success: false };
 
@@ -14,7 +11,7 @@ const setup = (props = {}) => {
 };
 
 test("renders without error", () => {
-  const wrapper = setup({ success: false });
+  const wrapper = setup();
   const component = findByTestAttr(wrapper, "component-congrats");
   expect(component.length).toBe(1);
 });
