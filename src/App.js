854a15143cbb0c7bf98f110ccfb875f7df263b32
MarcinBalejko <mbbm9@o2.pl>
2021-01-22 22:32:07 +0100

    :recycle: Manual acceptance testing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+import GuessedWords from "./GuessedWords";
+import Congrats from "./Congrats";
 
-export default App;
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Jotto</h1>
+        <Congrats success={true} />
+        <GuessedWords
+          guessedWords={[{ guessedWord: "train", letterMatchCount: 3 }]}
+        />
+      </div>
+    );
+  }
+}
