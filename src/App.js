d4db680afdd68991711853b523790ab8c2cfd9c4
MarcinBalejko <mbbm9@o2.pl>
2021-03-07 00:58:00 +0100

    Coded useReducer and useEffect to pass the tests

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from "react";
+import hookActions from "./actions/hookActions";
 import "./App.css";
 
+// Reducer to update state
+function reducer(state, action) {
+  switch (action.type) {
+    case "setSecretWord":
+      return { ...state, secretWord: action.payload };
+    default:
+      throw new Error(`Invalid action type: ${action.type}`);
+  }
+}
+
 export function App() {
+  const [state, dispatch] = React.useReducer(reducer, { secretWord: null });
+
+  const setSecretWord = (secretWord) => {
+    dispatch({ type: "setSecretWord", payload: secretWord });
+  };
+
+  React.useEffect(() => {
+    hookActions.getSecretWord(setSecretWord);
+  }, []);
+
   return <div data-test="component-app"></div>;
 }
 
